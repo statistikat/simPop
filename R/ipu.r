32ed2118d1f4679eb6fe56432c9ca155ee9d143f
bernhard <bernhard.meindl@statistik.gv.at>
2014-04-15 11:31:49 +0200

    small fixes and updates

--- R/ipu.r
+++ R/ipu.r
@@ -1,10 +1,21 @@
-ipu <- function(inp, con, eps=1e-07) {
-  # attach hhid later
-  hhid <- inp[,1,with=F]
-  inp <- inp[,-c(1), with=FALSE]  
+ipu <- function(inp, con, hid=NULL, eps=1e-07, verbose=FALSE) {
+  # check for existing hid and possibly generate it
+  # attach hhid later  
+  if ( is.null(hid) ) {
+    hhid <- 1:nrow(inp)
+  } else {
+    ii <- match(hid, colnames(inp))
+    hhid <- inp[,ii,with=F]
+    inp <- inp[,-c(ii), with=FALSE]      
+  }
   
+  # remove cols from inp not existing in con
+  inp <- inp[,match(names(con), colnames(inp)),with=F]
+  
+  # reorder constraints
+  con <- con[match(colnames(inp), names(con))]  
   if ( any(colnames(inp) != names(con)) ) {
-    stop("check your constraints!\n")
+    stop("constraints (con) does not match input (inp) -> check your input!\n")
   }
   
   # initialize weights
@@ -24,7 +35,9 @@ ipu <- function(inp, con, eps=1e-07) {
     gamma.vals.new <- sapply(1:length(con), function(x) { (abs(sum(w*inp[,x,with=F]) - con[[x]])) / con[[x]] })
     gamma.new <- mean(gamma.vals.new)
     delta <- abs(gamma.new - gamma)
-    cat("improvement in run",counter,":",delta," | gamma=",gamma.new,"\n")
+    if ( verbose ) {
+      cat("improvement in run",counter,":",delta," | gamma=",gamma.new,"\n")
+    }
     
     if ( gamma.new < eps || delta < eps ) {
       run_ind <- FALSE
@@ -33,7 +46,9 @@ ipu <- function(inp, con, eps=1e-07) {
       gamma <- gamma.new
     }
   }
-  cat("finished after",counter,"interations!\n")
+  if ( verbose ) {
+    cat("ipu finished after",counter,"interations!\n")
+  }  
   out <- cbind(hhid, inp)  
   out$weights <- w
   invisible(out)
