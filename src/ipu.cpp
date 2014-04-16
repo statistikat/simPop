c0ed5917162b9876a54082aa23c7917da9ba5826
bernhard <bernhard.meindl@statistik.gv.at>
2014-04-16 06:31:09 +0200

    ignore compiled files

---
+++ src/ipu.cpp
@@ -0,0 +1,16 @@
+#include <Rcpp.h>
+using namespace Rcpp;
+
+// Below is a simple example of exporting a C++ function to R. You can
+// source this function into an R session using the Rcpp::sourceCpp 
+// function (or via the Source button on the editor toolbar)
+
+// For more on using Rcpp click the Help button on the editor toolbar
+
+// [[Rcpp::export]]
+double ipu_work(NumericMatrix inp, List con) {
+   
+   
+   
+   
+}
