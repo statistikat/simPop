ee4644b115b9573645a90fb547d600fe01e4ec0e
bernhard <bernhard.meindl@statistik.gv.at>
2014-04-16 08:33:02 +0200

    Rcpp implementation of ipu which is much faster. update package for use of Rcpp

--- R/ipu.r
+++ R/ipu.r
@@ -1,55 +1,27 @@
 ipu <- function(inp, con, hid=NULL, eps=1e-07, verbose=FALSE) {
-  # check for existing hid and possibly generate it
-  # attach hhid later  
+  if ( class(inp) %in% c("data.frame", "data.table") ) {
+    cnames <- colnames(inp)
+    inp <- as.matrix(inp)
+    colnames(inp) <- cnames
+  }
+  
   if ( is.null(hid) ) {
     hhid <- 1:nrow(inp)
   } else {
     ii <- match(hid, colnames(inp))
-    hhid <- inp[,ii,with=F]
-    inp <- inp[,-c(ii), with=FALSE]      
-  }
-  
-  # remove cols from inp not existing in con
-  inp <- inp[,match(names(con), colnames(inp)),with=F]
-  
-  # reorder constraints
+    hhid <- inp[,ii]
+    inp <- inp[,-c(ii)]      
+  }  
+  inp <- inp[,match(names(con), colnames(inp))]
   con <- con[match(colnames(inp), names(con))]  
   if ( any(colnames(inp) != names(con)) ) {
-    stop("constraints (con) does not match input (inp) -> check your input!\n")
-  }
-  
-  # initialize weights
-  w <- rep(1,nrow(inp))  
-  gamma.vals <- sapply(1:length(con), function(x) { (abs(sum(w*inp[,x]) - con[[x]])) / con[[x]] })
-  gamma <- mean(gamma.vals)
-  
-  run_ind <- TRUE
-  counter <- 0
-  while ( run_ind ) {
-    counter <- counter + 1
-    for ( j in 1:length(con) ) {
-      v <- con[[j]] / sum(inp[,j, with=FALSE]*w, na.rm=TRUE)  
-      index <- which(inp[,j, with=F]!=0)
-      w[index] <- v*w[index]      
-    }    
-    gamma.vals.new <- sapply(1:length(con), function(x) { (abs(sum(w*inp[,x,with=F]) - con[[x]])) / con[[x]] })
-    gamma.new <- mean(gamma.vals.new)
-    delta <- abs(gamma.new - gamma)
-    if ( verbose ) {
-      cat("improvement in run",counter,":",delta," | gamma=",gamma.new,"\n")
-    }
-    
-    if ( gamma.new < eps || delta < eps ) {
-      run_ind <- FALSE
-    } else {
-      gamma.vals <- gamma.vals.new
-      gamma <- gamma.new
-    }
-  }
-  if ( verbose ) {
-    cat("ipu finished after",counter,"interations!\n")
+    stop("constraints (con) do not match input (inp) -> check your input!\n")
   }  
+  w <- as.numeric(rep(1,nrow(inp)))   
+  con <- as.numeric(unlist(con))
+  w <- .Call("synthPop_ipu_work", inp, con, w, eps=eps, verbose=ifelse(verbose, 1L, 0L), package="synthPop")
   out <- cbind(hhid, inp)  
+  out <- as.data.frame(out)
   out$weights <- w
-  invisible(out)
+  invisible(out)  
 }
--- src/ipu.cpp
+++ src/ipu.cpp
@@ -1,16 +1,81 @@
+#include <R.h>
+#include <cmath>
 #include <Rcpp.h>
 using namespace Rcpp;
 
-// Below is a simple example of exporting a C++ function to R. You can
-// source this function into an R session using the Rcpp::sourceCpp 
-// function (or via the Source button on the editor toolbar)
-
-// For more on using Rcpp click the Help button on the editor toolbar
-
 // [[Rcpp::export]]
-double ipu_work(NumericMatrix inp, List con) {
-   
-   
-   
-   
+NumericVector ipu_work(NumericMatrix inp, NumericVector con, NumericVector w, double eps, IntegerVector verbose) { 
+  int nr_con = con.size();  
+  int nr_rows = inp.nrow();
+  NumericVector gamma_vals(nr_con);  
+  NumericVector gamma_vals_new(nr_con);
+  double gamma, gamma_new, delta, v;
+  bool verb = false;
+  if ( verbose[0] == 1 ) {
+    verb = true;
+  }
+    
+  for ( int i=0; i < nr_con; ++i ) {
+    gamma_vals[i] = (fabs(sum(inp(_,i)*w)-con[i])) / con[i];
+  }  
+  gamma = mean(gamma_vals);
+  
+  bool run_ind = true;
+  int counter = 0;
+  while ( run_ind ) {
+    counter = counter + 1;
+    for ( int j=0; j < nr_con; ++j ) {
+      v = con[j] / sum(inp(_,j)*w);
+      for ( int k=0; k<nr_rows; ++k ) {
+        if ( inp(k,j) != 0 ) {
+          w[k] = v*w[k]; 
+        }
+      }
+    }    
+    
+    // recalculate gamma_vals
+    for ( int i=0; i < nr_con; ++i ) {
+      gamma_vals_new[i] = (fabs(sum(inp(_,i)*w)-con[i])) / con[i];
+    }  
+    gamma_new = mean(gamma_vals_new);
+        
+    delta = fabs(gamma_new - gamma);
+    if ( verb ) {
+      Rprintf("improvement in run %d: %g | gamma=%g\n", counter, delta, gamma_new);
+    }
+    
+    if ( gamma_new < eps | delta < eps ) {
+      run_ind = false;
+    } else {
+      for ( int k=0; k<nr_con; ++k ) {
+        gamma_vals[k] = gamma_vals_new[k];
+      }
+      gamma = gamma_new;
+    }
+  }
+  if ( verb ) {
+    Rprintf("ipu finished after %d interations!\n", counter);
+  }  
+  return(w);
+}
+
+
+// exporting for package (compileAttributes())
+NumericVector ipu_work(NumericMatrix inp, NumericVector con, NumericVector w, double eps, IntegerVector verbose);
+RcppExport SEXP synthPop_ipu_work(SEXP inpSEXP, SEXP conSEXP, SEXP wSEXP, SEXP epsSEXP, SEXP verboseSEXP) {
+BEGIN_RCPP
+  SEXP __sexp_result;
+  {
+      Rcpp::RNGScope __rngScope;
+      Rcpp::traits::input_parameter< NumericMatrix >::type inp(inpSEXP );
+      Rcpp::traits::input_parameter< NumericVector >::type con(conSEXP );
+      Rcpp::traits::input_parameter< NumericVector >::type w(wSEXP );
+      Rcpp::traits::input_parameter< double >::type eps(epsSEXP );
+      Rcpp::traits::input_parameter< IntegerVector >::type verbose(verboseSEXP );
+      NumericVector __result = ipu_work(inp, con, w, eps, verbose);
+      PROTECT(__sexp_result = Rcpp::wrap(__result));
+  }
+  UNPROTECT(1);
+  return __sexp_result;
+END_RCPP
 }
