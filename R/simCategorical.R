86face1a9dc5eb02ad22724551a2f02e03187e3e
bernhard <bernhard.meindl@statistik.gv.at>
2014-04-24 15:14:08 +0200

    use parallel-package; various fixes

--- R/simCategorical.R
+++ R/simCategorical.R
@@ -1,8 +1,20 @@
 # cur.var: current.variable (value of 'i' from outer loop)
-generate.values <- function(dataSample, dataPop, method, cur.var, excludeLevels, hasNewLevels, newLevels, w, formula.cmd, eps, limit, censor, levelsResponse) {
-  if( !nrow(dataSample) ) {
+generate.values <- function(dataSample, dataPop, params) {
+  if ( !nrow(dataSample) ) {
     return(character())
   }
+
+  meth <- params$method
+  cur.var <- params$cur.var
+  excludeLevels <- params$excludeLevels
+  hasNewLevels <- params$hasNewLevels
+  newLevels <- params$newLevels
+  w <- params$w
+  formula.cmd <- params$formula.cmd
+  eps <- params$eps
+  limit <- params$limit
+  censor <- params$censor
+  levelsResponse <- params$levelsResponse  
 
   indGrid <- split(1:nrow(dataPop), dataPop, drop=TRUE)
   grid <- dataPop[sapply(indGrid, function(i) i[1]), , drop=FALSE]
@@ -11,23 +23,21 @@ generate.values <- function(dataSample, dataPop, method, cur.var, excludeLevels,
   # model, hence population can have additional levels
   # these need to be removed since those probabilities cannot 
   # be predicted from the model
-  if( excludeLevels ) {
+  if ( excludeLevels ) {
     exclude <- mapply(function(pop, new) pop %in% new, 
       pop=grid[, hasNewLevels, drop=FALSE], 
       new=newLevels[hasNewLevels])
-    if(is.null(dim(exclude))) {
+    if ( is.null(dim(exclude)) ) {
       exclude <- which(any(exclude))
     } else exclude <- which(apply(exclude, 1, any))
-  } else exclude <- integer()
-
+  } else {
+    exclude <- integer()
+  }
   # fit multinomial model
   # command needs to be constructed as string
   # this is actually a pretty ugly way of fitting the model
   #browser()
-  tt <- Sys.time()
   mod <- eval(parse(text=formula.cmd))  # fitted model
-  tt <- Sys.time()-tt
-  print(tt)
 
   # predict probabilities
   if( length(exclude) == 0 ) {
@@ -42,17 +52,17 @@ generate.values <- function(dataSample, dataPop, method, cur.var, excludeLevels,
     }
   }   
   
-  if ( method %in% "multinom" ) {
+  if ( meth %in% "multinom" ) {
     probs <- predict(mod, newdata=newdata, type="probs")
   }
-  if ( method %in% "naivebayes" ) {
+  if ( meth %in% "naivebayes" ) {
     probs <- predict(mod, newdata=newdata, type="raw")
   }        
   # TODO: fix error if level-sets are not equal!
-  if ( method %in% "ctree" ) {
+  if ( meth %in% "ctree" ) {
     probs <- do.call("rbind", predict(mod, newdata=newdata, type="prob"))
   }
-  if ( method == "liblinear" ) {
+  if ( meth == "liblinear" ) {
     probs <- predict(mod, newx=newdata, proba=TRUE)
   }
 
@@ -76,13 +86,15 @@ generate.values <- function(dataSample, dataPop, method, cur.var, excludeLevels,
     }
   }
   # local function for sampling from probabilities
-  if( length(ind) == 1 ) {
+  if ( length(ind) == 1 ) {
     resample <- function(k, n, p) rep.int(1, n[k])
-  } else if(length(ind) == 2) {
+  } else if ( length(ind) == 2 ) {
     resample <- function(k, n, p) spSample(n[k], c(1-p[k],p[k]))
-  } else resample <- function(k, n, p) spSample(n[k], p[k,])
+  } else {
+    resample <- function(k, n, p) spSample(n[k], p[k,])
+  }
   # generate realizations for each combination
-  if(length(exclude) == 0) {
+  if ( length(exclude) == 0 ) {
     ncomb <- as.integer(sapply(indGrid, length))
     sim <- lapply(1:length(ncomb), resample, ncomb, probs)
   } else {
@@ -91,17 +103,21 @@ generate.values <- function(dataSample, dataPop, method, cur.var, excludeLevels,
     sim[-exclude] <- lapply(1:length(ncomb), resample, ncomb, probs)
   }
   sim <- unsplit(sim, dataPop, drop=TRUE)
-  # return realizations
-  levelsResponse[ind][sim]
+  invisible(levelsResponse[ind][sim])
 }
 
 # simulation of variables using random draws from the observed 
 # conditional distributions of their multivariate realizations
-generate.values.distribution <- function(dataSample, grid, dataPop, additional, basic, w) {
+generate.values.distribution <- function(dataSample, dataPop, params) {
+  grid <- params$grid
+  additional <- params$additional
+  basic <- params$basic
+  w <- params$w
+
   if( !nrow(dataSample) ) {
     return(character())
-  }  
-
+  }
+  
   # population data
   splitS <- split(1:nrow(dataSample), dataSample[, basic], drop=TRUE)
   pSplit <- lapply(splitS, function(i) {
@@ -130,61 +146,62 @@ simCategorical <- function(dataS, dataP, w = "rb050", strata = "db040",
   limit = NULL, censor = NULL, maxit = 500, 
   MaxNWts = 1500, eps = NULL, seed) {
   
-  if ( method == "ctree" ) {
-    stop("fix errow with unequal levels of factors in train and test-dataset!\n")
-  }
-  
   parallel <- FALSE
   if ( Sys.info()["sysname"] != "Windows" ) {
     nr_cores <- detectCores()
     if ( nr_cores > 2 ) {
       parallel <- TRUE
       nr_cores <- nr_cores-1 # keep one core available
-    }    
+    } else {
+      parallel <- FALSE
+    }  
   }  
-    
   ##### initializations
-  if( !missing(seed) ) {
+  if ( !missing(seed) ) {
     set.seed(seed)  # set seed of random number generator
   }
-  if( length(strata) != 1 ) { 
+  if ( length(strata) != 1 ) { 
     stop("currently 'strata' must specify exactly one column of 'data'")
   }
-  method <- match.arg(method)
+  method <- match.arg(method)  
+  if ( method == "ctree" ) {
+    stop("fix errow with unequal levels of factors in train and test-dataset!\n")
+  }  
   
-  if( missing(basic) ) {
+  if ( missing(basic) ) {
     basic <- c("age", "rb090")
-    if( method == "multinom") {
+    if ( method == "multinom" ) {
       basic <- c(basic, "hsize")
     }
   }
   varNames <- c(w=w, strata=strata, basic, additional)
 
-  time.prep <- Sys.time()
-
   # check data
-  if(all(varNames %in% names(dataS))) {
+  if ( all(varNames %in% names(dataS)) ) {
     dataS <- dataS[, varNames]
-  } else stop("undefined variables in the sample data")
-  if(!all(c(strata, basic) %in% names(dataP))) {
+  } else {
+    stop("undefined variables in the sample data")
+  }
+  if ( !all(c(strata, basic) %in% names(dataP)) ) {
     stop("undefined variables in the population data")
   }
 
   # observations with missings are excluded from simulation
   exclude <- getExclude(dataS)
-  if(length(exclude)) dataS <- dataS[-exclude,]
-
+  if ( length(exclude) ) {
+    dataS <- dataS[-exclude,]
+  }
   # variables are coerced to factors
   dataS <- checkFactor(dataS, c(strata, basic, additional))
   dataP <- checkFactor(dataP, c(strata, basic))
 
   # check arguments to account for structural zeros
-  if(length(additional) == 1) {
-    if(!(length(limit) == 1 && isTRUE(names(limit) == additional))) {
+  if ( length(additional) == 1 ) {
+    if ( !(length(limit) == 1 && isTRUE(names(limit) == additional)) ) {
       limit <- list(limit)
       names(limit) <- additional
     }
-    if(!(length(censor) == 1 && isTRUE(names(censor) == additional))) {
+    if ( !(length(censor) == 1 && isTRUE(names(censor) == additional)) ) {
       censor <- list(censor)
       names(censor) <- additional
     }
@@ -194,59 +211,39 @@ simCategorical <- function(dataS, dataP, w = "rb050", strata = "db040",
   N <- nrow(dataP)
   indStrata <- split(1:N, dataP[, strata, drop=FALSE])
 
-  time.prep <- Sys.time() - time.prep
-  cat("time.prep: "); print(time.prep)
-
   ##### simulation
   # predictor variables
   predNames <- basic  # names of predictor variables
   
   if ( method == "distribution" ) {
-    if ( parallel == TRUE ) {      
-      values <- tryCatch({
-        cl <- makePSOCKcluster(nr_cores)        
-        setDefaultCluster(cl)  
-        clusterExport(cl=cl, varlist=c("generate.values.distribution","dataS","grid",
-          "dataP","additional","basic","w","strata","tableWt",
-          "indStrata","spSample"), envir=environment())
-        levs <- levels(dataS[, strata])
-        values <- parLapply(cl, levs, function(x) { 
-          generate.values.distribution(
-            dataSample=dataS[dataS[, strata] == x, , drop=FALSE], 
-            grid=expand.grid(lapply(dataS[, additional], levels)),
-            dataPop=dataP[indStrata[[x]], , drop=FALSE], 
-            additional=additional,
-            basic=basic,
-            w=w
-          )        
-        })
-      },
-      error=function(){
-        stopCluster(cl) 
+    params <- list()
+    params$grid <- expand.grid(lapply(dataS[, additional], levels))
+    params$additional <- additional
+    params$basic <- basic
+    params$w <- w
+
+    if ( parallel ) {      
+      values <- mclapply(levels(dataS[, strata]), function(x) { 
+        generate.values.distribution(
+          dataSample=dataS[dataS[, strata] == x, , drop=FALSE],
+          dataPop=dataP[indStrata[[x]], , drop=FALSE], params)
       })
-      stopCluster(cl)
     } else {
       values <- lapply(levels(dataS[, strata]), function(x) { 
         generate.values.distribution(
           dataSample=dataS[dataS[, strata] == x, , drop=FALSE], 
-          grid=expand.grid(lapply(dataS[, additional], levels)),
-          dataPop=dataP[indStrata[[x]], , drop=FALSE], 
-          additional=additional,
-          basic=basic,
-          w=w
-        )
-      })          
+          dataPop=dataP[indStrata[[x]], , drop=FALSE], params)
+      })
     }
     values <- unsplit(values, dataP[, strata, drop=FALSE])
-    
+
     ## add new categorical variables to data set and return
-    dataP[, additional] <- values    
+    dataP[, additional] <- values
     return(dataP)
   }
-    
+
   # any other method
-  for(i in additional ) {
-    cat("i:",i,"\n")
+  for ( i in additional ) {
     # components of multinomial model are specified
     levelsResponse <- levels(dataS[, i])
 
@@ -255,7 +252,7 @@ simCategorical <- function(dataS, dataP, w = "rb050", strata = "db040",
       formula.cmd <- paste(i, "~", paste(predNames, collapse = " + "))
       formula.cmd <- paste("suppressWarnings(multinom(", formula.cmd, 
         ", weights=", w, ", data=dataSample, trace=FALSE", 
-        ", maxit=",maxit, ", MaxNWts=", MaxNWts,"))", sep="")					
+        ", maxit=",maxit, ", MaxNWts=", MaxNWts,"))", sep="")
     }
     # simulation via recursive partitioning and regression trees
     if ( method == "ctree" ) {
@@ -265,12 +262,10 @@ simCategorical <- function(dataS, dataP, w = "rb050", strata = "db040",
     if ( method == "naivebayes" ) {
       formula.cmd <- paste(i, "~", paste(predNames, collapse = " + "))
       formula.cmd <- paste("naiveBayes(", formula.cmd, ", data=dataSample, usekernel=TRUE)", sep="")						
-    }	
+    }
     if ( method == "liblinear" ) {
       formula.cmd <- paste("LiblineaR(data=as.matrix(dataSample[,c('",paste(predNames, collapse="','"),"')]), labels=dataSample[,cur.var], type=0, bias=TRUE,verbose=FALSE)", collapse="", sep="")						
-    }				
-
-    time.levs <- Sys.time()
+    }
 
     # check if population data contains factor levels that do not exist 
     # in the sample
@@ -281,76 +276,39 @@ simCategorical <- function(dataS, dataP, w = "rb050", strata = "db040",
     })
     hasNewLevels <- sapply(newLevels, length) > 0
     excludeLevels <- any(hasNewLevels)
-    time.levs <- Sys.time() - time.levs
-    
+
     # generate values of new variable
-    tt <- Sys.time()
+    params <- list()
+    params$method <- method
+    params$cur.var <- i
+    params$excludeLevels <- excludeLevels
+    params$hasNewLevels <- hasNewLevels
+    params$newLevels <- newLevels
+    params$w <- w
+    params$formula.cmd <- formula.cmd
+    params$eps <- eps
+    params$limit <- limit
+    params$censor <- censor
+    params$levelsResponse <- levelsResponse
     if ( parallel ) {
-      values <- tryCatch({
-        # make cluster        
-        cl <- makePSOCKcluster(nr_cores)        
-        setDefaultCluster(cl)  
-        clusterExport(cl=cl, varlist=c("generate.values","dataS","strata",
-          "dataP","predNames","indStrata","excludeLevels","hasNewLevels",
-          "newLevels","formula.cmd","method","eps","i",
-          "limit","censor","spSample","levelsResponse","multinom","ctree",
-          "naiveBayes","LiblineaR"), envir=environment())   
-        values <- parLapply(cl, levels(dataS[, strata]), function(x) {
-          generate.values(
-            dataSample=dataS[dataS[, strata] == x, , drop=FALSE], 
-            dataPop=dataP[indStrata[[x]], predNames, drop=FALSE],
-            method=method,
-            cur.var=i,
-            excludeLevels=excludeLevels, 
-            hasNewLevels=hasNewLevels, 
-            newLevels=newLevels,
-            w=w,
-            formula.cmd=formula.cmd,
-            eps=eps,
-            limit=limit,
-            censor=censor,
-            levelsResponse=levelsResponse
-          )
-        })
-        stopCluster(cl)
-        values
-      },
-      error=function(cond) {
-        print("error!\n")
-        stopCluster(cl)   
+      values <- mclapply(levels(dataS[, strata]), function(x) {
+        generate.values(
+          dataSample=dataS[dataS[, strata] == x, , drop=FALSE],
+          dataPop=dataP[indStrata[[x]], predNames, drop=FALSE], params
+        )        
       })
     } else {
-      cat("no parallel!\n")
-      values <- lapply(levels(dataS[, strata]), function(x) { 
+      values <- lapply(levels(dataS[, strata]), function(x) {
         generate.values(
-          dataSample=dataS[dataS[, strata] == x, , drop=FALSE], 
-          dataPop=dataP[indStrata[[x]], predNames, drop=FALSE],
-          method=method,
-          cur.var=i,
-          excludeLevels=excludeLevels, 
-          hasNewLevels=hasNewLevels, 
-          newLevels=newLevels,
-          w=w,
-          formula.cmd=formula.cmd,
-          eps=eps,
-          limit=limit,
-          censor=censor,
-          levelsResponse=levelsResponse
+          dataSample=dataS[dataS[, strata] == x, , drop=FALSE],
+          dataPop=dataP[indStrata[[x]], predNames, drop=FALSE], params
         )
-      })  
-    }      
-        
-    tt <- Sys.time() - tt
-    cat("time.levs: "); print(time.levs)
-    cat("time.values: "); print(tt)
-  
+      })
+    }
     values <- factor(unsplit(values, dataP[, strata, drop=FALSE]), levels=levelsResponse)
-
     ## add new categorical variable to data set
     dataP[, i] <- values
     predNames <- c(predNames, i)
   }
-
-  # return simulated data
-  return(dataP)
+  invisible(dataP)
 }
