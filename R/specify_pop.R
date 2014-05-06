3b7459f45480324eb6643e198649c51e8d38f2d6
bernhard <bernhard.meindl@statistik.gv.at>
2014-05-06 11:55:27 +0200

    adjust for new slots pid and hhsize

--- R/specify_pop.R
+++ R/specify_pop.R
@@ -1,15 +1,36 @@
-specify_sample <- function(data, hhid, strata=NULL) {
-  if ( !class(hhid)=="character" | length(hhid) != 1 | is.na(match(hhid, colnames(samp)))) {
+specify_pop <- function(data, hhid, hhsize=NULL, pid=NULL, strata=NULL) {
+  if ( !class(hhid)=="character" | length(hhid) != 1 | is.na(match(hhid, colnames(data)))) {
     stop("hhid must be a character defining the variable holding household ids and must be of length 1!\n")
   }
   if ( !is.null(strata) ) {
-    if ( !class(strata)=="character" | length(strata) != 1 | is.na(match(strata, colnames(samp)))) {
+    if ( !class(strata)=="character" | length(strata) != 1 | is.na(match(strata, colnames(data)))) {
       stop("strata must be a character defining the variable holding information on stratas and must be of length 1!\n")
     }
   }
 
   data <- as.data.table(data)
   setkeyv(data, hhid)
-  invisible(new("popObj", data=data, hhid=hhid, strata=strata))
+
+  if ( !is.null(hhsize) ) {
+    if ( !class(hhsize)=="character" | length(hhsize) != 1 | is.na(match(hhsize, colnames(data)))) {
+      stop("strata must be a character defining the variable holding information on stratas and must be of length 1!\n")
+    } 
+  } else {
+    hhsize <- "hhsize"
+    sizes <- data[,.N,by=key(data)]
+    setnames(sizes, c(key(data), hhsize))
+    data <- data[sizes]
+  }
+  if ( !is.null(pid) ) {
+    if ( !class(pid)=="character" | length(pid) != 1 | is.na(match(pid, colnames(data)))) {
+      stop("strata must be a character defining the variable holding information on stratas and must be of length 1!\n")
+    }
+  } else {
+    pid <- "pid"
+    sizes <- data[,.N,by=key(data)]
+    setnames(sizes, c(key(data), hhsize))
+    data$pid <- paste(data[,hhid], ".",unlist(sapply(sizes$hhsize, function(x) { seq(1, x) })), sep="")
+  }
+  invisible(new("popObj", data=data, hhid=hhid, hhsize=hhsize, pid=pid, strata=strata))
 }
 
--- R/specify_sample.R
+++ R/specify_sample.R
@@ -1,18 +1,39 @@
-specify_sample <- function(data, hhid, weight, strata=NULL) {
-  if ( !class(hhid)=="character" | length(hhid) != 1 | is.na(match(hhid, colnames(samp)))) {
+specify_sample <- function(data, hhid, hhsize=NULL, pid=NULL, weight, strata=NULL) {
+  if ( !class(hhid)=="character" | length(hhid) != 1 | is.na(match(hhid, colnames(data)))) {
     stop("hhid must be a character defining the variable holding household ids and must be of length 1!\n")
   }
-  if ( !class(weight)=="character" | length(weight) != 1 | is.na(match(weight, colnames(samp)))) {
+  if ( !class(weight)=="character" | length(weight) != 1 | is.na(match(weight, colnames(data)))) {
     stop("weight must be a character defining the variable holding sampling weights and must be of length 1!\n")
   }
   if ( !is.null(strata) ) {
-    if ( !class(strata)=="character" | length(strata) != 1 | is.na(match(strata, colnames(samp)))) {
+    if ( !class(strata)=="character" | length(strata) != 1 | is.na(match(strata, colnames(data)))) {
       stop("strata must be a character defining the variable holding information on stratas and must be of length 1!\n")
     }
   }
 
   data <- as.data.table(data)
   setkeyv(data, hhid)
-  invisible(new("sampleObj", data=data, hhid=hhid, weight=weight, strata=strata))
+
+  if ( !is.null(hhsize) ) {
+    if ( !class(hhsize)=="character" | length(hhsize) != 1 | is.na(match(hhsize, colnames(data)))) {
+      stop("strata must be a character defining the variable holding information on stratas and must be of length 1!\n")
+    } 
+  } else {
+    hhsize <- "hhsize"
+    sizes <- data[,.N,by=key(data)]
+    setnames(sizes, c(key(data), hhsize))
+    data <- data[sizes]
+  }
+  if ( !is.null(pid) ) {
+    if ( !class(pid)=="character" | length(pid) != 1 | is.na(match(pid, colnames(data)))) {
+      stop("strata must be a character defining the variable holding information on stratas and must be of length 1!\n")
+    }
+  } else {
+    pid <- "pid"
+    sizes <- data[,.N,by=key(data)]
+    setnames(sizes, c(key(data), hhsize))
+    data$pid <- paste(data[,hhid], ".",unlist(sapply(sizes$hhsize, function(x) { seq(1, x) })), sep="")
+  }
+  invisible(new("sampleObj", data=data, hhid=hhid, hhsize=hhsize, pid=pid, weight=weight, strata=strata))
 }
 
