deac04e04e4d2ff6da4302a9b3477e1c2e931e58
bernhard <bernhard.meindl@data-analysis.at>
2014-05-06 17:37:17 +0200

    c++ implementation of calibVars()

--- R/calibVars.R
+++ R/calibVars.R
@@ -1,59 +1,21 @@
-# ---------------------------------------
-# Author: Andreas Alfons
-#         Vienna University of Technology
-# ---------------------------------------
-
-#' Construct a matrix of binary variables for calibration
-#' 
-#' Construct a matrix of binary variables for calibration of sample weights
-#' according to known marginal population totals.
-#' 
-#' @name calibVars
-#' @param x a vector that can be interpreted as factor, or a matrix or
-#' \code{data.frame} consisting of such variables.
-#' 
-#' @return A matrix of binary variables that indicate membership to the
-#' corresponding factor levels.
-#' 
-#' @author Andreas Alfons
-#' 
-#' @seealso \code{\link{calibSample}}
-#' 
-#' @keywords survey
-#' 
-#' @examples
-#' data(eusilc)
-#' # default method
-#' aux <- calibVars(eusilc$rb090)
-#' head(aux)
-#' # data.frame method
-#' aux <- calibVars(eusilc[, c("db040", "rb090")])
-#' head(aux)
-#' 
-#' @export
-
 calibVars <- function(x) UseMethod("calibVars")
-
-#' @S3method calibVars default
 calibVars.default <- function(x) {
-    if(length(x) == 0) matrix(integer(), 0, 0)
-    x <- as.factor(x)
-    res <- sapply(levels(x), function(l) as.integer(x == l))
-    rownames(res) <- names(x)  # set rownames from original vector
-    res
+  if(length(x) == 0) matrix(integer(), 0, 0)
+  x <- as.factor(x)
+  levs <- levels(x)
+  res <- .Call("synthPop_binary_representation", levels=1:length(levels(x)), values=as.integer(x), package="synthPop")
+  colnames(res) <- levs
+  rownames(res) <- names(x)  # set rownames from original vector
+  res    
 }
-
-#' @S3method calibVars matrix
 calibVars.matrix <- function(x) calibVars(as.data.frame(x))
-
-#' @S3method calibVars data.frame
 calibVars.data.frame <- function(x) {
-    res <- lapply(x, calibVars)  # list of matrices for each variable
-    res <- mapply(function(x, nam) {
-            colnames(x) <- paste(nam, colnames(x), sep=".")
-            x
-        }, res, names(x), SIMPLIFY=FALSE)
-    res <- do.call("cbind", res)  # combine matrices
-    rownames(res) <- row.names(x)  # set rownames from original data.frame
-    res
+  res <- lapply(x, calibVars)  # list of matrices for each variable
+  res <- mapply(function(x, nam) {
+    colnames(x) <- paste(nam, colnames(x), sep=".")
+    x
+  }, res, names(x), SIMPLIFY=FALSE)
+  res <- do.call("cbind", res)
+  rownames(res) <- row.names(x)
+  res
 }
---
+++ src/calibVars.cpp
@@ -0,0 +1,32 @@
+#include <R.h>
+#include <Rcpp.h>
+using namespace Rcpp;
+
+// [[Rcpp::export]]
+IntegerMatrix binary_representation(IntegerVector levels, IntegerVector values) {
+  IntegerMatrix out(values.size(), levels.size());
+  for ( int j=0; j<levels.size(); ++j ) {
+    for ( int i=0; i<values.size(); ++i ) {
+      if ( levels[j] == values[i] ) {
+        out(i,j) = 1;
+      }
+    }
+  }
+  return(out);
+}
+
+IntegerMatrix binary_representation(IntegerVector levels, IntegerVector values);
+RcppExport SEXP synthPop_binary_representation(SEXP levelsSEXP, SEXP valuesSEXP) {
+BEGIN_RCPP
+    SEXP __sexp_result;
+    {
+        Rcpp::RNGScope __rngScope;
+        Rcpp::traits::input_parameter< IntegerVector >::type levels(levelsSEXP );
+        Rcpp::traits::input_parameter< IntegerVector >::type values(valuesSEXP );
+        IntegerMatrix __result = binary_representation(levels, values);
+        PROTECT(__sexp_result = Rcpp::wrap(__result));
+    }
+    UNPROTECT(1);
+    return __sexp_result;
+END_RCPP
+}
