411527ec473c82003855342473190f92de3e9080
bernhard <bernhard.meindl@data-analysis.at>
2014-05-15 15:16:13 +0200

    cosmetics

--- R/spPredict.R
+++ R/spPredict.R
@@ -1,11 +1,9 @@
-# ---------------------------------------
-# Author: Stefan Kraft
-#         Vienna University of Technology
-# ---------------------------------------
-
 spPredict <- function(mod, newdata, intercept = FALSE) {
-    #beta <- mod$coef
-    beta <- coef(mod)
-    if(intercept) X <- cbind(1 , newdata) else X <- newdata 
-    as.numeric(X %*% beta)
+  beta <- coef(mod)
+  if ( intercept ) {
+    X <- cbind(1 , newdata)
+  } else {
+    X <- newdata
+  }
+  as.numeric(X %*% beta)
 }
--- R/spSample.R
+++ R/spSample.R
@@ -1,12 +1,6 @@
-# ---------------------------------------
-# Author: Stefan Kraft
-#         Vienna University of Technology
-# ---------------------------------------
-
-# 'sample' gives warning for alias sampling due to incompatible results with 
+# 'sample' gives warning for alias sampling due to incompatible results with
 # earlier versions of R
-
 spSample <- function(n, p) {
-#    suppressWarnings(sample(length(p), size=n, replace=TRUE, prob=p))
-    sample(length(p), size=n, replace=TRUE, prob=p)
+  #suppressWarnings(sample(length(p), size=n, replace=TRUE, prob=p))
+  sample(length(p), size=n, replace=TRUE, prob=p)
 }
