fcbfc47dd5991bffe03b0215113eb20099d7a4e1
templ <matthias.templ@gmail.com>
2014-07-19 12:28:45 +0200

    show

--- R/printFunctions.R
+++ R/printFunctions.R
@@ -4,24 +4,33 @@ setMethod("show", "dataObj",
               dname <- "population"
             } else {
               dname <- "survey sample"
-            }        
+            }     
+            cat("\n -------------- \n")
             cat(dname, "of size", nrow(object@data), "x", ncol(object@data), "\n")
+            cat("\n Selected important variables: \n")
             cat("\n household ID:", object@hhid)
             cat("\n personal ID:", object@pid)
             cat("\n variable household size:", object@hhsize)
             cat("\n sampling weight:", object@weight)
             cat("\n strata:", object@strata)
+            cat("\n -------------- \n")
+            cat("\n")
           }
 )
 
 setMethod("show", "synthPopObj",
           function(object){
-              dname <- "population"
-            cat(dname, " of size", nrow(object@data), "x", ncol(object@data), "\n")
-            cat("\n household ID:", object@hhid)
-            cat("\n personal ID:", object@pid)
-            cat("\n variable household size:", object@hhsize)
-            cat("\n sampling weight:", object@weight)
-            cat("\n strata:", object@strata)
+            dname <- "synthetic population"
+            cat("\n")
+            cat("-------------- \n")
+            cat(dname, " of size \n", nrow(object@pop@data), "x", ncol(object@pop@data), "\n")
+            cat("\n") 
+            cat("build from a sample of size \n")
+            cat(nrow(object@sample@data),"x",ncol(object@sample@data))
+            cat("\n")
+            cat("-------------- \n\n")
+            cat("variables in the population:\n")
+            cat(paste(colnames(object@pop@data), collapse=","))
+            cat("\n")
           }
 )
