47fa77854dc7cb210bdbd68e2c38d34ced33fdf5
bernhard <bernhard.meindl@statistik.gv.at>
2014-12-19 12:57:32 +0100

    fix bug in calibPop.cpp

--- src/calibPop.cpp
+++ src/calibPop.cpp
@@ -52,7 +52,7 @@ IntegerVector generate_new_solution(IntegerVector weights, IntegerVector hh_ids,
   for ( int i=0; i < nr_draws; ++i ) {
     // select an active household and set it to inactive
     //s = rand() % nr_active;     // range: 0 to nr_active
-    ss = floor(runif(1,0,nr_active+1));     // range: 0 to nr_active
+    ss = floor(runif(1,0,nr_active));     // range: 0 to nr_active
     s = ss[1];
     if(s > nr_active)
       s = nr_active;
@@ -60,7 +60,7 @@ IntegerVector generate_new_solution(IntegerVector weights, IntegerVector hh_ids,
 
     // select an inactive household and set it to active
     //s = rand() % nr_inactive;   // range: 0 to nr_inactive
-    ss = floor(runif(1,0,nr_inactive+1));
+    ss = floor(runif(1,0,nr_inactive));
     s = ss[1];
     if(s > nr_inactive)
           s = nr_inactive;
