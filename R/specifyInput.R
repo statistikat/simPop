3176145efc1a46770467500064b6074013e70ef2
alexkowa <alex@kowarik.net>
2016-01-24 18:21:03 +0100

    parameter for population to start with a census

--- R/specifyInput.R
+++ R/specifyInput.R
@@ -20,6 +20,8 @@
 #' @param strata character vector of length 1 specifing variable name within
 #' slot \code{data} of variable holding information on strata, e.g. regions or
 #' NULL if such variable does not exist.
+#' @param population TRUE/FALSE vector of length 1 specifing if the data object is a sample or a population
+#' NULL if such variable does not exist.
 #' @export
 #' @author Bernhard Meindl
 #' @keywords method
@@ -28,12 +30,18 @@
 #' inp <- specifyInput(data=eusilcS, hhid="db030", weight="rb050", strata="db040")
 #' class(inp)
 #' inp
-specifyInput <- function(data, hhid, hhsize=NULL, pid=NULL, weight, strata=NULL) {
+specifyInput <- function(data, hhid, hhsize=NULL, pid=NULL, weight=NULL, strata=NULL, population=FALSE) {
   if ( !class(hhid)=="character" | length(hhid) != 1 | is.na(match(hhid, colnames(data)))) {
     stop("hhid must be a character defining the variable holding household ids and must be of length 1!\n")
   }
-  if ( !class(weight)=="character" | length(weight) != 1 | is.na(match(weight, colnames(data)))) {
-    stop("weight must be a character defining the variable holding sampling weights and must be of length 1!\n")
+  ## check if weight var is defined well for sample data
+  if(!population){
+    if ( !class(weight)=="character" | length(weight) != 1 | is.na(match(weight, colnames(data)))) {
+      stop("weight must be a character defining the variable holding sampling weights and must be of length 1!\n")
+    }
+  }else{ # for population data no weight car is accepted
+    if(!is.null(weight))
+      stop("weight must not be defined for a population")
   }
   if ( !is.null(strata) ) {
     if ( !class(strata)=="character" | length(strata) != 1 | is.na(match(strata, colnames(data)))) {
@@ -63,6 +71,6 @@ specifyInput <- function(data, hhid, hhsize=NULL, pid=NULL, weight, strata=NULL)
     sizes <- data[,.N,by=key(data)]
     data$pid <- paste(data[[hhid]], ".",unlist(sapply(sizes[["N"]], function(x) { seq(1, x) })), sep="")
   }
-  invisible(new("dataObj", data=data, hhid=hhid, hhsize=hhsize, pid=pid, weight=weight, strata=strata, ispopulation=FALSE))
+  invisible(new("dataObj", data=data, hhid=hhid, hhsize=hhsize, pid=pid, weight=weight, strata=strata, ispopulation=population))
 }
 
