684627ead8d836b029247f63c1f1cdbc3b0169ec
Alexander Kowarik <alex@kowarik.net>
2017-03-07 09:47:46 +0100

    setDTthreads(1) to avoid double parallelization

--- R/zzz.R
+++ R/zzz.R
@@ -1,3 +1,5 @@
 .onAttach <- function(lib, pkg) {
   packageStartupMessage(paste("Package simPop",utils::packageVersion("simPop"),"has been loaded!\n"))
+  packageStartupMessage("Since simPop does explicit parallelization,\n the number of data.table threads is set to 1.")
+  setDTthreads(1)
 }
