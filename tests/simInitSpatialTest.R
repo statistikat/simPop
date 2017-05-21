3f65ef46e1c31a2aa1ec33e8b0e243740293978e
alexkowa <alex@kowarik.net>
2017-05-21 15:24:09 +0200

    some basic checking of the outputs in the test

--- tests/simInitSpatialTest.R
+++ tests/simInitSpatialTest.R
@@ -47,11 +47,34 @@ simPopObj <- simStructure(data=inp, method="direct", basicHHvars=c("age", "gende
 # use only HH counts
   simPopObj1 <- simInitSpatial(simPopObj, additional="district", region="db040", tspatialHH=tabHH,
       tspatialP=NULL)
+  test1 <- merge(tabHH,simPopObj1@pop@data[!duplicated(db030),.N,by=district],by="district")
+  if(test1[,max(abs(Freq-N)/Freq)]>0.01){
+    stop("Test should result in perfect distribution of the households")
+  }
   
+  simPopObj <- simStructure(data=inp, method="direct", basicHHvars=c("age", "gender"))
 # use only P counts
   simPopObj2 <- simInitSpatial(simPopObj, additional="district", region="db040", tspatialHH=NULL,
       tspatialP=tabP)
+  test2 <- merge(tabP,simPopObj2@pop@data[,.N,by=district],by="district")
+  test2[abs(Freq-N)/Freq>0.05,.N]
+  test2[,summary(abs(Freq-N)/Freq)]
   
+  if(!test2[abs(Freq-N)/Freq>0.05,.N]<10){
+    stop("Test should result in good distribution of the persons")
+  }
+  
+  simPopObj <- simStructure(data=inp, method="direct", basicHHvars=c("age", "gender"))
 # use P and HH counts
   simPopObj3 <- simInitSpatial(simPopObj, additional="district", region="db040", tspatialHH=tabHH,
       tspatialP=tabP)
+test3 <- merge(tabHH[,.(db040,district,FreqH=Freq)],
+    merge(tabP,simPopObj3@pop@data[,.(np=.N,nh=sum(!duplicated(db030))),by=district],by="district"),by="district")
+if(test3[,max(abs(Freq-N)/Freq)]>0.01){
+  stop("Test should result in perfect distribution of the households")
+}
+
+test3[abs(Freq-np)/Freq>0.05,.N]
+if(!test3[abs(Freq-np)/Freq>0.05,.N]<10){
+  stop("Test should result in good distribution of the persons")
+}
