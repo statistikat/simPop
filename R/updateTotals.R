2b4f42a219155c071e241122b8e646f4cbbf684a
Alexander Kowarik <alex@kowarik.net>
2019-11-22 22:12:21 +0100

    minimal faster

--- R/updateTotals.R
+++ R/updateTotals.R
@@ -1,19 +1,16 @@
 # update totals0 with population totals
 updateTotals <- function(totals0,data0,hhid,numberPop="weight_choose"){
   firstPersonInHousehold <- NULL
-  for(i in 1:length(totals0)){
-    
-    byVars <- intersect(colnames(totals0[[i]]),colnames(data0))
-    if(grepl("pers",names(totals0[i]))){
-      totals0[[i]] <- merge(totals0[[i]][,mget(c(byVars,"Freq"))],
-                            data0[,list(FreqPop=sum(get(numberPop))),by=c(byVars)],
-                            by=c(byVars))
-    }else{
-      totals0[[i]] <- merge(totals0[[i]][,mget(c(byVars,"Freq"))],
-                            data0[firstPersonInHousehold==TRUE,list(FreqPop=sum(get(numberPop))),by=c(byVars)],
-                            by=c(byVars))
-    }
-  }
-  
+  cn <- colnames(data0)
+  TF <- grepl("pers",names(totals0))
+  totals0[TF] <- lapply(totals0[TF], function(t0){
+    byVars <- intersect(colnames(t0),cn)
+    data0[,list(FreqPop=sum(get(numberPop))),by=c(byVars)][t0[,mget(c(byVars,"Freq"))],
+                          on=c(byVars)]
+  })
+  totals0[!TF] <- lapply(totals0[!TF], function(t0){
+    byVars <- intersect(colnames(t0),cn)
+    data0[firstPersonInHousehold==TRUE,list(FreqPop=sum(get(numberPop))),by=c(byVars)][t0[,mget(c(byVars,"Freq"))],on=c(byVars)]
+    })
   return(totals0)
 }
